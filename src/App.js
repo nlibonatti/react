c9d277f334dd9adbe76ced0e73a13d4b7f3435a5
nlibonatti <106163604+nlibonatti@users.noreply.github.com>
2022-10-27 09:35:20 -0300

    NavBar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import NavBar from './components/NavBar'
 import ItemListContainer from './components/ItemListContainer' 
-
+import './index.css'
 
 const App = () =>{
   return (
     <>
     <NavBar />
-    <ItemListContainer greeting="Bienvenido al ecomm" />
+    <ItemListContainer greeting="Bienvenido al sitio de compra de criptomonedas" />
     </>
   )
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,15 @@
 
 import CartWidget from '../components/CartWidget' 
+import { TbCurrencyBitcoin } from 'react-icons/tb'
+import '../index.css'
 
 const NavBar = () =>{
   return (
     <>
-        <nav class="navbar navbar-expand-lg bg-light">
+    <body> 
+        <nav class="navbar navbar-expand-lg bg-secondary">
           <div class="container-fluid">
-            <a class="navbar-brand" href="#">Cripto Comm</a>
+            <a class="navbar-brand fs-3" href="#"> <TbCurrencyBitcoin />Cripto Comm</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
@@ -28,12 +31,12 @@ const NavBar = () =>{
                   <a class="nav-link d-flex" href="#"></a>
                 </li>
               </ul>
-              <ul>
-                <h1 class="d-flex"><CartWidget /></h1>
-              </ul>  
-            </div>
+             </div>
+             <h1 class="d-flex"><CartWidget /></h1>
+
           </div>
-        </nav>  
+        </nav>
+      </body>
     </>
   )
 }
