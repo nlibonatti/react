5debc08df789f81ea479c8bd410153ded948e328
nlibonatti <106163604+nlibonatti@users.noreply.github.com>
2022-11-10 22:47:14 -0300

    Item_details

--- src/components/CoinDetail.js
+++ src/components/CoinDetail.js
@@ -8,14 +8,14 @@ const DetailCoin = ({item}) => {
                 <img src={item.image} className="card-img-top" alt={item.name}/>
                 <div className="card-body">
                 <h5 className="card-title">{item.name}</h5>
-                <p className="card-text">{item.name}</p>
+                <p className="card-text">{item.description}</p>
                 </div>
                 <ul className="list-group list-group-flush">
                   <li className="list-group-item">Price: {item.cost}</li>
                   <li className="list-group-item">Stock: {item.stock}</li>
                 </ul>
                 <div className="card-body">
-                  <a href="/" className="card-link">Add</a>
+                  <a href="/" className="card-link">Add to Cart</a>
                   <a href="/" className="card-link">Rate</a>
                 </div>
                 </div>
